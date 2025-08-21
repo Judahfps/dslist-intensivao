0766097b7b1dd679e9b18368e9b654c0ee009877
Guilherme Silva <sebastian_49@hotmail.com>
2025-08-20 22:16:05 -0400

    first commit

---
+++ src/main/java/com/guilherme/dslist_intensivao/DslistIntensivaoApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.dslist_intensivao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistIntensivaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistIntensivaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/dslist_intensivao/DslistIntensivaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.dslist_intensivao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistIntensivaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
